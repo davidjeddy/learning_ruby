ff597f784944d522d907d7cb03c52ff07a57bd2a
davidjeddy <me@davidjeddy.com>
2018-12-17 14:29:04 -0500

    Added excersize 4,5, and 6.

---
+++ learnrubythehardway/mystuff/ex6.rb
@@ -0,0 +1,20 @@
+type_of_people = 10
+x = 'There are #{type_of_people} types of people.'
+binary = 'binary'
+do_not = 'don\'t'
+y = 'Those who know #{binary} and those who #{do_not}.'
+
+puts x
+puts y
+
+puts 'I said: #{x}.'
+puts 'I also said: '#{y}'.'
+
+hilarious = false
+joke_evaluation = 'Isn\'t that joke so funny?! #{hilarious}'
+
+puts joke_evaluation
+
+w = 'This is th left side of...'
+e = 'a string wiuth a right side.'
+puts w + e
