7b91943da84fdeff3d5f75afc73650178387c830
davidjeddy <me@davidjeddy.com>
2018-12-20 14:21:27 -0500

    UPDATED lexicon lesson ex48

--- learn-ruby-the-hard-way/ex48/lexicon.rb
+++ learn-ruby-the-hard-way/ex48/lexicon.rb
@@ -1,6 +1,36 @@
 class Lexicon
 
   def Lexicon.scan(direction)
-    [['direction', direction]]
+    return_array = []
+
+    direction = direction.split(' ')
+
+    direction.each do |each|
+      
+      # lowercase strings
+      each = each.downcase
+
+      if each == "north" || each =="east" || each == "south" || each == "west"
+        # directions
+        return_array.push(['direction', each]); next
+      elsif each == "go" || each == "kill" || each == "eat"
+        # verb
+        return_array.push(['verb', each]); next
+      elsif each == "the" || each == "in" || each == "of"
+        # stop
+        return_array.push(['stop', each]); next
+      elsif each == "bear" || each == "princess"
+        # noun
+        return_array.push(['noun', each]); next
+      elsif each.to_i > 0
+        # number
+        return_array.push(['number', each.to_i]); next
+      end
+
+      # error
+      return_array.push(['error', each])
+    end
+    
+    return return_array
   end
 end
--- learn-ruby-the-hard-way/ex48/tests/test_lexicon.rb
+++ learn-ruby-the-hard-way/ex48/tests/test_lexicon.rb
@@ -5,52 +5,51 @@ class TestLexicon < Test::Unit::TestCase
 
   def test_directions()
     assert_equal(Lexicon.scan("north"), [['direction', 'north']])
-    result = Lexicon.scan("north south east")
 
+    result = Lexicon.scan("north south east")
     assert_equal(result, [['direction', 'north'],
            ['direction', 'south'],
            ['direction', 'east']])
   end
 
-  # def test_verbs()
-  #   assert_equal(Lexicon.scan("go"), [['verb', 'go']])
-  #   result = Lexicon.scan("go kill eat")
-  #   assert_equal(result, [['verb', 'go'],
-  #          ['verb', 'kill'],
-  #          ['verb', 'eat']])
-  # end
+  def test_verbs()
+    assert_equal(Lexicon.scan("go"), [['verb', 'go']])
+    result = Lexicon.scan("go kill eat")
+    assert_equal(result, [['verb', 'go'],
+           ['verb', 'kill'],
+           ['verb', 'eat']])
+  end
 
+  def test_stops()
+    assert_equal(Lexicon.scan("the"), [['stop', 'the']])
+    result = Lexicon.scan("the in of")
+    assert_equal(result, [['stop', 'the'],
+           ['stop', 'in'],
+           ['stop', 'of']])
+  end
 
-  # def test_stops()
-  #   assert_equal(Lexicon.scan("the"), [['stop', 'the']])
-  #   result = Lexicon.scan("the in of")
-  #   assert_equal(result, [['stop', 'the'],
-  #          ['stop', 'in'],
-  #          ['stop', 'of']])
-  # end
+  def test_nouns()
+    assert_equal(Lexicon.scan("bear"), [['noun', 'bear']])
+    result = Lexicon.scan("bear princess")
+    assert_equal(result, [['noun', 'bear'],
+           ['noun', 'princess']])
+  end
 
+  def test_numbers()
+    assert_equal(Lexicon.scan("1234"), [['number', 1234]])
+    result = Lexicon.scan("3 91234")
+    assert_equal(result, [['number', 3],
+           ['number', 91234]])
 
-  # def test_nouns()
-  #   assert_equal(Lexicon.scan("bear"), [['noun', 'bear']])
-  #   result = Lexicon.scan("bear princess")
-  #   assert_equal(result, [['noun', 'bear'],
-  #          ['noun', 'princess']])
-  # end
+    # assert_equal(Lexicon.scan("0"), [['number', 0]])
+  end
 
-  # def test_numbers()
-  #   assert_equal(Lexicon.scan("1234"), [['number', 1234]])
-  #   result = Lexicon.scan("3 91234")
-  #   assert_equal(result, [['number', 3],
-  #          ['number', 91234]])
-  # end
-
-
-  # def test_errors()
-  #   assert_equal(Lexicon.scan("ASDFADFASDF"), [['error', 'ASDFADFASDF']])
-  #   result = Lexicon.scan("bear IAS princess")
-  #   assert_equal(result, [['noun', 'bear'],
-  #          ['error', 'IAS'],
-  #          ['noun', 'princess']])
-  # end
+  def test_errors()
+    assert_equal(Lexicon.scan("ASDFASDFASDF"), [['error', 'asdfasdfasdf']])
+    result = Lexicon.scan("bear IAS princess")
+    assert_equal(result, [['noun', 'bear'],
+           ['error', 'ias'],
+           ['noun', 'princess']])
+  end
 
 end
